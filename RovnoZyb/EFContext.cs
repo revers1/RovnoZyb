9f00be163bb170b3923188b9ede0e0455261ec51
romap <romap@lan>
2020-04-24 19:12:57 +0300

    first commit

---
+++ RovnoZyb/EFContext.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RovnoZyb.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RovnoZyb
+{
+    public class EFContext : IdentityDbContext<User>
+    {
+        public EFContext(DbContextOptions<EFContext> options) : base(options)
+        {
+
+        }
+        public DbSet<UserMoreInfo> userMoreInfos { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)//Zamena Foreign key 
+        {
+            builder.Entity<User>()
+                .HasOne(u => u.UserMoreInfo)
+                .WithOne(t => t.User)
+                .HasForeignKey<UserMoreInfo>(ui => ui.id);
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
